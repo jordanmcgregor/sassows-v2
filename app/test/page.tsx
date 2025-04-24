e0ad692c57b766e47bb28c3a017bfcf3b92d8054
jmmcgregor <jordanmmcgregor@gmail.com>
2025-04-24 14:46:26 -0600

    changed out the opengraph image and the twitter image and the metadata

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+import { IconFlower } from "@tabler/icons-react";
+
+export default function Test() {
+    return (
+        <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-60 items-center justify-center rounded-4xl">
+                <IconFlower className="size-40" />
+            </div>
+            Sassows
+        </a>
+    )
+}
