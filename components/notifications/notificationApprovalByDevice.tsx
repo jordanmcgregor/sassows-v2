9cf8bd50f26e31aab5078d367bbcde3110477b3b
jmmcgregor <jordanmmcgregor@gmail.com>
2025-06-13 16:08:58 -0600

    reimplemented the original code base

--- components/notifications/notificationApprovalByDevice.tsx
+++ components/notifications/notificationApprovalByDevice.tsx
@@ -14,7 +14,7 @@ import { toast } from "sonner";
 import { createClient } from '@/utils/supabase/client';
 import { useUser } from "@/context/selected-child";
 
-async function getNotificationPermissionAndFCMToken() {
+async function getNotificationPermissionAndToken() {
     // Step 1: Check if Notifications are supported in the browser.
     if (!("Notification" in window)) {
         console.info("This browser does not support desktop notification");
@@ -23,16 +23,18 @@ async function getNotificationPermissionAndFCMToken() {
 
     // Step 2: Check if permission is already granted.
     if (Notification.permission === "granted") {
-        return await fetchToken(); // Fetch the token
+        return await fetchToken();
     }
 
     // Step 3: If permission is not denied, request permission from the user.
     if (Notification.permission !== "denied") {
         const permission = await Notification.requestPermission();
         if (permission === "granted") {
-            return await fetchToken(); // Fetch the token
+            return await fetchToken();
         }
     }
+
+    console.log("Notification permission not granted.");
     return null;
 }
 
@@ -48,85 +50,146 @@ export default function NotificationApprovalByDevice({ os, browser }: { os: any,
     const router = useRouter();
 
     const loadToken = async () => {
+        // Step 4: Prevent multiple fetches if already fetched or in progress.
         if (isLoading.current) return;
-        isLoading.current = true;
+
+        isLoading.current = true; // Mark loading as in progress.
         setIsSubmitting(true)
-        const fetchedToken = await getNotificationPermissionAndFCMToken();
+        const token = await getNotificationPermissionAndToken(); // Fetch the token.
 
-
-
+        // Step 5: Handle the case where permission is denied.
         if (Notification.permission === "denied") {
             setNotificationPermissionStatus("denied");
-            setToken(null);
-        } else if (fetchedToken) {
-            setNotificationPermissionStatus("granted");
-            setToken(fetchedToken);
-            // // Store the token in Supabase
-            // const { data, error } = await supabase.auth.getUser()
-            // if (data?.user) {
-            //     const { error } = await supabase
-            //         .from('users') // Replace with your table name
-            //         .update({ fcm_token: fetchedToken }) // Replace 'your_user_id'
-            //         .eq('user_id', data.user.id)
-            //     return fetchedToken
-            // }
-            // if (error) {
-            //     console.error('Error storing FCM token:', error.message);
-            // }
-        } else {
-            setNotificationPermissionStatus(Notification.permission as NotificationPermission);
-            setToken(null);
+            console.info(
+                "%cPush Notifications issue - permission denied",
+                "color: green; background: #c7c7c7; padding: 8px; font-size: 20px"
+            );
+            isLoading.current = false;
+            return;
         }
-    };
 
-    const handleApproveNotifications = async () => {
-        const token = await loadToken(); // This waits for the user's permission action
-        // Check if the user granted permission
-        const supabase = createClient();
-        const { data: auth } = await supabase.auth.getUser();
-        if (auth?.user) {
-            await supabase
+        // Step 6: Retry fetching the token if necessary. (up to 3 times)
+        // This step is typical initially as the service worker may not be ready/installed yet.
+        if (!token) {
+            if (retryLoadToken.current >= 3) {
+                alert("Unable to load token, refresh the browser");
+                console.info(
+                    "%cPush Notifications issue - unable to load token after 3 retries",
+                    "color: green; background: #c7c7c7; padding: 8px; font-size: 20px"
+                );
+                isLoading.current = false;
+                return;
+            }
+
+            retryLoadToken.current += 1;
+            console.error("An error occurred while retrieving token. Retrying...");
+            isLoading.current = false;
+            await loadToken();
+            return;
+        }
+
+        // Step 7: Set the fetched token and mark as fetched.
+        setNotificationPermissionStatus(Notification.permission);
+        setToken(token);
+        // ---------------------------------------------------------------------
+        // --------------------- Add The Token To Supabase ---------------------
+        // ---------------------------------------------------------------------
+        const supabase = await createClient();
+        const { data: { user } } = await supabase.auth.getUser()
+        if (user) {
+            const { data: tokenData, error: tokenError } = await supabase
                 .from('users')
-                .update({ onboarded: true, fcm_token: token })
-                .eq('user_id', auth.user.id);
+                .update({ fcm_token: token })  // You must include user id to update existing user
+                .eq('user_id', user.id)
+                .select();
+            if (tokenError) {
+                // handle error
+                console.error('Error updating token:', tokenError);
+            }
         }
-        router.push('/home');
-        // if (Notification.permission === "granted" && token) {
-        //     const supabase = createClient();
-        //     const { data: auth } = await supabase.auth.getUser();
-
-        //     if (auth?.user) {
-        //         await supabase
-        //             .from('users')
-        //             .update({ onboarded: true })
-        //             .eq('user_id', auth.user.id);
-        //     }
-        //     isLoading.current = false;
-        //     router.push('/home');
-        // } else {
-        //     // Optionally show a message/toast if denied
-        //     router.push('/home');
-        //     console.warn("Notification permission not granted.");
-        // }
+        isLoading.current = false;
     };
 
     // useEffect(() => {
-    //     const markOnboardingComplete = async () => {
-    //         if (notificationPermissionStatus) {
-    //             const supabase = createClient();
-
-    //             const { data: auth, error: authError } = await supabase.auth.getUser()
-    //             if (auth && auth.user) {
-    //                 const { error } = await supabase
-    //                     .from('users') // Replace with your table name
-    //                     .update({ onboarded: true }) // Replace 'your_user_id'
-    //                     .eq('user_id', auth.user.id)
-    //             }
-    //             router.push('/home')
-    //         }
+    //     // Step 8: Initialize token loading when the component mounts.
+    //     if ("Notification" in window) {
+    //         loadToken();
     //     }
-    //     markOnboardingComplete()
-    // }, [notificationPermissionStatus])
+    // }, []);
+
+    useEffect(() => {
+        const setupListener = async () => {
+            if (!token) return; // Exit if no token is available.
+
+            console.log(`onMessage registered with token ${token}`);
+            const m = await messaging();
+            if (!m) return;
+
+            // Step 9: Register a listener for incoming FCM messages.
+            const unsubscribe = onMessage(m, (payload) => {
+                if (Notification.permission !== "granted") return;
+
+                console.log("Foreground push notification received:", payload);
+                const link = payload.fcmOptions?.link || payload.data?.link;
+
+                if (link) {
+                    toast.info(
+                        `${payload.notification?.title}: ${payload.notification?.body}`,
+                        {
+                            action: {
+                                label: "Visit",
+                                onClick: () => {
+                                    const link = payload.fcmOptions?.link || payload.data?.link;
+                                    if (link) {
+                                        router.push(link);
+                                    }
+                                },
+                            },
+                        }
+                    );
+                } else {
+                    toast.info(
+                        `${payload.notification?.title}: ${payload.notification?.body}`
+                    );
+                }
+
+                // --------------------------------------------
+                // Disable this if you only want toast notifications.
+                const n = new Notification(
+                    payload.notification?.title || "New message",
+                    {
+                        body: payload.notification?.body || "This is a new message",
+                        data: link ? { url: link } : undefined,
+                    }
+                );
+
+                // Step 10: Handle notification click event to navigate to a link if present.
+                n.onclick = (event) => {
+                    event.preventDefault();
+                    const link = (event.target as any)?.data?.url;
+                    if (link) {
+                        router.push(link);
+                    } else {
+                        console.log("No link found in the notification payload");
+                    }
+                };
+                // --------------------------------------------
+            });
+
+            return unsubscribe;
+        };
+
+        let unsubscribe: Unsubscribe | null = null;
+
+        setupListener().then((unsub) => {
+            if (unsub) {
+                unsubscribe = unsub;
+            }
+        });
+
+        // Step 11: Cleanup the listener when the component unmounts.
+        return () => unsubscribe?.();
+    }, [token, router, toast]);
     return (
         // <div className="bg-[url('/notificationpositioning.png')] bg-cover bg-center">
         <>
@@ -146,11 +209,11 @@ export default function NotificationApprovalByDevice({ os, browser }: { os: any,
                         repeat={0}
                     />
                 </div>
-                {os == 'iOS' ? <Apple loadToken={handleApproveNotifications} /> : null}
-                {os == 'Android' ? <Android loadToken={handleApproveNotifications} /> : null}
+                {os == 'iOS' ? <Apple loadToken={loadToken} /> : null}
+                {os == 'Android' ? <Android loadToken={loadToken} /> : null}
                 {/* <Android loadToken={loadToken} /> */}
                 <div className="flex-1 flex items-end">
-                    <Button onClick={handleApproveNotifications} variant={"default"} className="w-full h-12">
+                    <Button onClick={loadToken} variant={"default"} className="w-full h-12">
                         Help me remember the magic
                     </Button>
                 </div>
