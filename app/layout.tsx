a69d86a5f403c08382e395cbb21027eb6d0a5eaa
jmmcgregor <jordanmmcgregor@gmail.com>
2025-07-30 08:56:44 -0600

    added in vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MetaPixel from "@/components/meta/pixel";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -228,6 +229,7 @@ export default function RootLayout({
       >
         {children}
         <MetaPixel />
+        <Analytics />
       </body>
     </html>
   );
